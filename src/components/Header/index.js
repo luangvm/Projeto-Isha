7064a62692b41255fa799e222eaf3461571187e4
luangvm <luangabrielvm7@gmail.com>
2024-10-01 21:38:03 -0300

    feat: Recuperação de senha

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,70 +1,56 @@
 import styles from "../Header/Header.module.css";
 import fundo from "../../assets/LOGOTIPO 4.png";
-import {FaUser, FaLock} from "react-icons/fa";
+import { FaUser, FaLock } from "react-icons/fa";
 import { useState } from "react";
 import { TbBackground } from "react-icons/tb";
 import { MdCheckBox } from "react-icons/md";
 
 function Header() {
+  const [username, setUsername] = useState("");
 
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-  
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Username:", username);
+    alert("E-mail enviado ao e-mail informado: " + username);
+  };
 
-    const handleSubmit = (event) => {
-      event.preventDefault();
-      console.log("Username:", username);
-      console.log("Password:", password);
-      alert("Enviando Os Dados:" + username  + password);
-    };
-
-
-
-
-    return(
-
+  return (
     <div className={styles.fundo}>
+      <div className={styles.form}>
+        <header>
+          <form onSubmit={handleSubmit} className={styles.vidro}>
+            <h1>Recuperação de Senha</h1>
+            <div>
+              <input
+                className={styles.user}
+                type="email"
+                placeholder="E-mail Da Conta"
+                onChange={(e) => setUsername(e.target.value)}
+              />
+              <FaUser className={styles.icon2}></FaUser>
+            </div>
+            <button className={styles.botao} type="submit">
+              Recuperar
+            </button>
 
-        <div className={styles.form}>
-            <header>
-                <form onSubmit={handleSubmit} className={styles.vidro}>
-                        <h1>Acesse o Sistema</h1>
-                        <div>
-                            <input className={styles.user}
-                                type="email" 
-                                placeholder="E-mail" 
-                                onChange={(e) => setUsername(e.target.value)}>
-                            </input>
-                            <FaUser className={styles.icon2}></FaUser>
-                        </div>
-                        <div>
-                            <input className={styles.user}
-                                type="password" 
-                                placeholder="Senha" 
-                                onChange={(e) => setPassword(e.target.value)}>
-                            </input>
-                            <FaLock className={styles.icon}></FaLock>
-                        </div>
-                        <input type= "checkbox" className= {styles.recal} ></input> Lembre de mim
-
-                    <button className={styles.botao}>Entrar</button>
-
-                    <div className= {styles.recal}>
-                    <p className= {styles.recal} >Não tem uma conta ?<a href="#" className= {styles.recal}> Registrar </a></p>
-                        
-                        <p><a href="#" className= {styles.recal}> Esqueci a senha </a></p>
-
-                    </div>
-                </form>
-
-            </header>
-        </div>
-
+            <div className={styles.recal}>
+              <p className={styles.recal}>
+                Não tem uma conta?{" "}
+                <a href="#" className={styles.recal}>
+                  Registrar
+                </a>
+              </p>
+              <p>
+                <a href="#" className={styles.recal}>
+                  Esqueci a senha
+                </a>
+              </p>
+            </div>
+          </form>
+        </header>
+      </div>
     </div>
-
-    )
-    
-
+  );
 }
 
 export default Header;
